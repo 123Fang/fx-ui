c6c6845debb5f747dcb40f205210a1fb238011b1
123Fang <38906235+123Fang@users.noreply.github.com>
2023-09-15 14:00:42 +0800

    更新version

--- packages/index.js
+++ packages/index.js
@@ -21,7 +21,7 @@ const install = function (Vue, opts = {
   }
 }
 export default {
-  "version": "0.2.1",
+  "version": "0.2.0",
   // 导出的对象必须具有 install，才能被 Vue.use() 方法安装
   install,
 	// 以下是具体的组件列表
