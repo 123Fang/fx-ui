87c67137b26d2903c3dd44ce6eac7f25497ace57
123Fang <38906235+123Fang@users.noreply.github.com>
2023-09-18 17:01:06 +0800

    gc脚本添加自动生成doc目录和文件

--- scripts/gc.js
+++ scripts/gc.js
@@ -81,7 +81,98 @@ export default f${INDEXJSName}
 `
 fs.writeFileSync(path.join(DIRNAME, 'index.js'), indexJsContent);
 
-console.log(`创建组件${NORMALIZED_NAME}成功`)
 
 
+// 生成组件doc目录和其中的文件
+const DIRNAM_DOC = path.join(DIRNAME, 'doc')
+if (fs.existsSync(DIRNAM_DOC)) {
+  console.log(`${NAME} 组件doc目录已经存在！`);
+  process.exit(1);
+}
+fs.mkdirSync(DIRNAM_DOC, { recursive: true }); // 创建doc目录
+
+
+
+const demoContext = `<!-- html -->
+<template>
+  <fx-${NORMALIZED_NAME}></fx-${NORMALIZED_NAME}>
+</template>
+
+<!-- js -->
+<script setup>
+import { reactive,ref } from "vue";
+// init
+</script>
+`
+fs.writeFileSync(path.join(DIRNAM_DOC, 'demo1.vue'), demoContext);
+
+const attrTableContext = `
+<template>
+  <fx-table :options="options" size="small"></fx-table>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+const state = reactive({
+  options: {
+    fileds: [
+      { field: "attr", title: "参数", align: "center" },
+      { field: "type", title: "类型", align: "center" },
+      { field: "red", title: "说明", align: "center", width: "350px" },
+      { field: "sel", title: "可选值", align: "center" },
+      { field: "def", title: "默认值", align: "center" },
+    ],
+    datas: [
+      {
+        attr: "title",
+        type: "String",
+        red: "**",
+        sel: "——",
+        def: "标题",
+      },
+    ],
+  },
+});
+const { options } = state;
+</script>
+
+<style lang="scss" scoped>
+</style>
+`
+fs.writeFileSync(path.join(DIRNAM_DOC, 'Attributes.vue'), attrTableContext);
+
+
+const docContext = `
+<script setup>
+import demo1 from './demo1.vue'
+import Attributes from './Attributes.vue'
+import preview from '@/components/preview.vue'
+</script>
+
+# ${NAME} 组件
+
+#### ${NAME}组件基本描述...
+
+<br/>
+
+## 基本使用
+
+#### ${NAME} 按钮的基本使用
+
+<br/>
+<div class="componetnsBox">
+  <demo1/>
+</div>
+<preview compName="${NAME}" demoName="demo1"/>
+
+<br/>
+
+## Attributes 参数
+
+<Attributes/>
+<br/>
+`
+fs.writeFileSync(path.join(DIRNAM_DOC, 'doc.md'), docContext);
+
+console.log(`创建组件${NORMALIZED_NAME}成功，模版已生成`)
 
