9f14a512e6c1e6fd838d70d1455bf0eeb6c77ace
123Fang <38906235+123Fang@users.noreply.github.com>
2024-04-26 11:39:03 +0800

    fix: 格式

--- packages/tree/index.vue
+++ packages/tree/index.vue
@@ -1,43 +1,46 @@
 <template>
-  <div :class="['fx-tree-content-box',customClass]">
-    <template v-for="(item,index) in options" :key="index">
-      <fx-tree-item  :items="item" :data-key="index" :icon="icon" :defaultOpenNodes="defaultOpenNodes" @nodeClick="nClick($event)" :options="options" :index="0" @change="changeKey($event)" :tabIndexs="tabKey" @selectClick="emit('selectClick',$event)" :multiple="multiple" :defaultSelectNodes="defaultSelectNodes"></fx-tree-item>
+  <div :class="['fx-tree-content-box', customClass]">
+    <template v-for="(item, index) in options" :key="index">
+      <fx-tree-item :items="item" :data-key="index" :icon="icon" :defaultOpenNodes="defaultOpenNodes"
+        @nodeClick="nClick($event)" :options="options" :index="0" @change="changeKey($event)" :tabIndexs="tabKey"
+        @selectClick="emit('selectClick', $event)" :multiple="multiple"
+        :defaultSelectNodes="defaultSelectNodes"></fx-tree-item>
     </template>
   </div>
 </template>
 
 <script setup>
-import {ref} from 'vue'
+import { ref } from 'vue'
 import fxTreeItem from '../treeItem/index.js'
 defineOptions({
-  name:"fx-tree"
+  name: "fx-tree"
 })
-const emit = defineEmits(['nodeClick','selectClick'])
+const emit = defineEmits(['nodeClick', 'selectClick'])
 const props = defineProps({
-  options:{
-    type:Array,
-    default:()=>{
+  options: {
+    type: Array,
+    default: () => {
       return []
     }
   },
-  icon:String,
-  defaultOpenNodes:Array,
-  customClass:String,
-  multiple:Boolean,
-  defaultSelectNodes:{
-    type:Array,
-    default:()=>[]
+  icon: String,
+  defaultOpenNodes: Array,
+  customClass: String,
+  multiple: Boolean,
+  defaultSelectNodes: {
+    type: Array,
+    default: () => []
   }
 })
 
-props.options.forEach((item,index)=>{
+props.options.forEach((item, index) => {
   item.key = index.toString()
 })
 const tabKey = ref('')
-const changeKey = (e) =>{
+const changeKey = (e) => {
   tabKey.value = e
 }
 const nClick = (item) => {
-  emit('nodeClick',item)
+  emit('nodeClick', item)
 }
 </script>
